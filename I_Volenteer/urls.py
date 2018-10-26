b9fa744a13a695b68fff62bb3674b1d4c709c4c7
Abhilash V <abhilashnitk3@gmail.com>
2018-10-26 19:19:10 +0530

    Update urls.py

--- I_Volenteer/urls.py
+++ I_Volenteer/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views 
 
 urlpatterns = [
     path('',include('NLP.urls')),
     path('admin/', admin.site.urls),
+    path('hi/',views.hello)
 ]
